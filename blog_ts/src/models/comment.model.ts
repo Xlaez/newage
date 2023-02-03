9b39aebb31dbcd92727a1b9f41c6ee9f840209fc
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2023-02-03 12:14:43 +0100

    Posts, comment and category

---
+++ blog_ts/src/models/comment.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema, model, Document, Model } from 'mongoose';
+import { paginate } from 'mongoose-paginate-v2';
+
+type CommentDocument = Document & {
+   body: string
+}
+
+const CommentSchema = new Schema(
+   {
+      body: {
+         type: Schema.Types.ObjectId,
+         required: true,
+       },
+       author: {
+         type: Schema.Types.ObjectId,
+         ref: 'users',
+       },
+       postId: {
+         type: Schema.Types.ObjectId,
+         ref: 'posts',
+       },
+       parentId: {
+         type: Schema.Types.ObjectId,
+         ref: 'users',
+       },
+       likes: {
+         type: Number,
+         default: 0,
+       },
+       replyCount: {
+         type: Number,
+         default: 0,
+       },
+     },
+     { timestamps: true }
+);
+
+const Comment: Model<CommentDocument> = mongoose.model<CommentDocument>('Comment', CommentSchema)
+export {
+   Comment,
+   CommentDocument
+};
---
+++ blog_ts/src/services/post.service.ts
@@ -0,0 +1,1 @@
+import { AppRes } from "owl-factory";
